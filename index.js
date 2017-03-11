ebb106a495aed05af4f8ee2df2a041a64e366f80
Kai Tamkun <ktamkun@gmail.com>
2017-03-11 00:42:56 -0800

    Cursor doesn't blink when the cursor is hidden (e.g., through ANSI escape codes); changed formatting

--- index.js
+++ index.js
@@ -1,87 +1,93 @@
-const throttle = require('lodash.throttle')
+const throttle = require("lodash.throttle")
 
-const BUSY_TIMEOUT = 700  // ms
-const BUSY_THROTTLE = BUSY_TIMEOUT / 2
+const BUSY_TIMEOUT = 700;
+const BUSY_THROTTLE = BUSY_TIMEOUT / 2;
 
-module.exports.decorateConfig = config => {
-  return Object.assign({}, config, {
-    termCSS: `
-      ${config.termCSS || ''}
-      .cursor-node[focus=true]:not([hyper-blink-moving]) {
-        animation: blink 1s ease infinite;
-      }
-      @keyframes blink {
-        10%, 50% { opacity: 0 }
-        60%, 100% { opacity: 1 }
-      }
-    `
-  })
-}
+module.exports.decorateConfig = (config) => {
+	return Object.assign({ }, config, { termCSS: `
+		${config.termCSS || ""}
 
-module.exports.decorateTerm = (Term, {React, notify}) => {
-  return class extends React.Component {
-    constructor (props, context) {
-      super(props, context)
-      this._onTerminal = this._onTerminal.bind(this)
-      this._onCursorChange = this._onCursorChange.bind(this)
-      this._updateCursorStatus = this._updateCursorStatus.bind(this)
-      this._markBusyThrottled = throttle(this._markBusy.bind(this), BUSY_THROTTLE)
-      this._markIdle = this._markIdle.bind(this)
-    }
+		.cursor-node[focus=true]:not([hyper-blink-moving]):not([hyper-blink-hidden]) {
+			animation: blink 1s ease infinite;
+		}
 
-    _onTerminal (term) {
-      /* eslint-disable prop-types */
-      if (this.props.onTerminal) {
-        this.props.onTerminal(term)
-      }
+		@keyframes blink {
+			10%, 50% {
+				opacity: 0;
+			}
 
-      this._cursor = term.cursorNode_
+			60%, 100% {
+				opacity: 1;
+			}
+		}
+	` });
+};
 
-      /* global MutationObserver */
-      this._observer = new MutationObserver(this._onCursorChange)
-      this._observer.observe(this._cursor, {
-        attributes: true,
-        childList: false,
-        characterData: false
-      })
-    }
+module.exports.decorateTerm = (Term, { React, notify }) => {
+	return class extends React.Component {
+		constructor(props, context) {
+			super(props, context);
+			this._onTerminal = this._onTerminal.bind(this);
+			this._onCursorChange = this._onCursorChange.bind(this);
+			this._updateCursorStatus = this._updateCursorStatus.bind(this);
+			this._markBusyThrottled = throttle(this._markBusy.bind(this), BUSY_THROTTLE);
+			this._markIdle = this._markIdle.bind(this);
+		};
 
-    _onCursorChange (mutations) {
-      const cursorMoved = mutations.some(m => m.attributeName === 'title')
-      if (cursorMoved) {
-        this._updateCursorStatus()
-      }
-    }
+		_onTerminal(term) {
+			if (this.props.onTerminal) {
+				this.props.onTerminal(term);
+			};
 
-    _updateCursorStatus () {
-      this._markBusyThrottled()
+			this._cursor = term.cursorNode_;
+			this._observer = new MutationObserver(this._onCursorChange);
+			this._observer.observe(this._cursor, {
+				attributes: true,
+				childList: false,
+				characterData: false,
+				attributeOldValue: true,
+				attributeFilter: ["style", "title"]
+			});
+		};
 
-      clearTimeout(this._markingTimer)
-      this._markingTimer = setTimeout(() => {
-        this._markIdle()
-      }, BUSY_TIMEOUT)
-    }
+		_onCursorChange(mutations) {
+			for (let mutation of mutations) {
+				if (mutation.attributeName == "style") {
+					let newOpacity = mutation.target.style.opacity,
+						oldOpacity = (mutation.oldValue.match(/opacity: (\d)/) || ["", "1"])[1];
+					if (oldOpacity == "0" && newOpacity == "1") {
+						this._cursor.removeAttribute("hyper-blink-hidden");
+					} else if (oldOpacity == "1" && newOpacity == "0") {
+						this._cursor.setAttribute("hyper-blink-hidden", true);
+					};
+				} else if (mutation.attributeName == "title") {
+					this._updateCursorStatus();
+				};
+			};
+		};
 
-    _markBusy () {
-      // console.log('!!! Cursor Moving !!!')
-      this._cursor.setAttribute('hyper-blink-moving', true)
-    }
+		_updateCursorStatus() {
+			this._markBusyThrottled();
+			clearTimeout(this._markingTimer);
+			this._markingTimer = setTimeout(this._markIdle, BUSY_TIMEOUT);
+		};
 
-    _markIdle () {
-      // console.log('...idle...')
-      this._cursor.removeAttribute('hyper-blink-moving')
-    }
+		_markBusy() {
+			this._cursor.setAttribute("hyper-blink-moving", true);
+		};
 
-    render () {
-      return React.createElement(Term, Object.assign({}, this.props, {
-        onTerminal: this._onTerminal
-      }))
-    }
+		_markIdle() {
+			this._cursor.removeAttribute("hyper-blink-moving");
+		};
 
-    componentWillUnmount () {
-      if (this._observer) {
-        this._observer.disconnect()
-      }
-    }
-  }
-}
+		render() {
+			return React.createElement(Term, Object.assign({ }, this.props, { onTerminal: this._onTerminal }));
+		};
+
+		componentWillUnmount() {
+			if (this._observer) {
+				this._observer.disconnect();
+			};
+		};
+	};
+};
